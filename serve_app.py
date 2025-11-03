0481b8cd7fe24758b05ca8cc92a75aae35fb0118
iris <nguyendung7219@gmail.com>
2025-11-03 15:15:31 +0700

    Add files via upload

---
+++ serve_app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify, render_template
+from backend_fake_news_multimodal import analyze_text
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/analyze', methods=['POST'])
+def analyze():
+    data = request.get_json()
+    text = data.get('text', '')
+    result = analyze_text(text)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
